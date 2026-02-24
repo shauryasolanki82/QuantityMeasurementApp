51ba2c5993fa23208cd92abe3a1c8ebc53de7415
shauryasolanki82 <chshauryasolanki@gmail.com>
2026-02-24 11:20:58 +0530

    [Shaurya] Add. Added to UC2 of QuantityMeasurement

--- src/main/java/com/shaurya/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/shaurya/quantitymeasurement/QuantityMeasurementApp.java
@@ -3,7 +3,24 @@ package com.shaurya.quantitymeasurement;
 import java.util.Objects;
 
 public class QuantityMeasurementApp {
-	
+	public static void demonstrateFeetEquality() {
+		Feet feet1=new Feet(88.0);
+		Feet feet2=new Feet(88.0);
+		if(feet1.equals(feet2)) {
+			System.out.println("Both quantity(Feet) are equal (True)");
+		}else {
+			System.out.println("Both quantity(Feet) are not equal (False)");
+		}
+	}
+	public static void demonstrateInchesEquality() {
+		Inches inch1=new Inches(10.0);
+		Inches inch2=new Inches(10.0);
+		if(inch1.equals(inch2)) {
+			System.out.println("Both quantity(Inches) are equal (True)");
+		}else {
+			System.out.println("Both quantity(Inches) are not equal (False)");
+		}
+	}
 	public static class Feet{
 		private final double value;
 		
@@ -26,13 +43,28 @@ public class QuantityMeasurementApp {
 	}
 
 	public static void main(String[] args) {
-		Feet feet1=new Feet(88.0);
-		Feet feet2=new Feet(88.0);
-		if(feet1.equals(feet2)) {
-			System.out.println("Both quantity are equal (True)");
-		}else {
-			System.out.println("Both quantity are not equal (False)");
+		demonstrateFeetEquality();
+		demonstrateInchesEquality();
+	}
+	public static class Inches{
+private final double value;
+		
+		public Inches(double value) {
+			this.value=value;
 		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if(this==obj) return true;
+			if(obj==null || getClass()!=obj.getClass()) return false;
+			Inches other=(Inches) obj;
+			return Double.compare(this.value, other.value)==0;
+		}
+		
+		@Override
+		public int hashCode() {
+			return Objects.hash(value);
+		}
+	}	
 	}
 
-}
--- src/test/java/com/shaurya/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/shaurya/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class QuantityMeasurementAppTest {
 
 	@Test
-	void testEquality_SameValue() {
+	void testFeetEquality_SameValue() {
 		QuantityMeasurementApp.Feet f1=new QuantityMeasurementApp.Feet(1.0);
 		QuantityMeasurementApp.Feet f2=new QuantityMeasurementApp.Feet(1.0);
 		
@@ -14,7 +14,7 @@ class QuantityMeasurementAppTest {
 	}
 	
 	@Test
-	void testEquality_DifferentValue() {
+	void testFeetEquality_DifferentValue() {
 		QuantityMeasurementApp.Feet f1=new QuantityMeasurementApp.Feet(68.0);
 		QuantityMeasurementApp.Feet f2=new QuantityMeasurementApp.Feet(88.0);
 		
@@ -22,30 +22,77 @@ class QuantityMeasurementAppTest {
 	}
 	
 	@Test
-	void testEquality_NullComparison() {
+	void testFeetEquality_NullComparison() {
 		QuantityMeasurementApp.Feet f1=new QuantityMeasurementApp.Feet(68.0);
 		
 		assertFalse(f1.equals(null));
 	}
 	
 	@Test
-	void testEquality_NonNumericInput() {
+	void testFeetEquality_NonNumericInput() {
 		QuantityMeasurementApp.Feet f1=new QuantityMeasurementApp.Feet(68.0);
 		
 		assertFalse(f1.equals("68"));
 	}
 	
 	@Test
-	void testEquality_SameReference() {
+	void testFeetEquality_SameReference() {
 		QuantityMeasurementApp.Feet f1=new QuantityMeasurementApp.Feet(68.0);
 		
 		assertTrue(f1.equals(f1));
 	}
 	
 	@Test
-    void testEquality_Consistent() {
+    void testFeetEquality_Consistent() {
 		QuantityMeasurementApp.Feet f1 = new QuantityMeasurementApp.Feet(1.0);
 		QuantityMeasurementApp.Feet f2 = new QuantityMeasurementApp.Feet(1.0);
+
+        assertTrue(f1.equals(f2));
+        assertTrue(f1.equals(f2));
+        assertTrue(f1.equals(f2));
+    }
+
+	@Test
+	void testInchEquality_SameValue() {
+		QuantityMeasurementApp.Inches f1=new QuantityMeasurementApp.Inches(1.0);
+		QuantityMeasurementApp.Inches f2=new QuantityMeasurementApp.Inches(1.0);
+		
+		assertTrue(f1.equals(f2));
+	}
+	
+	@Test
+	void testInchEquality_DifferentValue() {
+		QuantityMeasurementApp.Inches f1=new QuantityMeasurementApp.Inches(68.0);
+		QuantityMeasurementApp.Inches f2=new QuantityMeasurementApp.Inches(88.0);
+		
+		assertFalse(f1.equals(f2));
+	}
+	
+	@Test
+	void testInchEquality_NullComparison() {
+		QuantityMeasurementApp.Inches f1=new QuantityMeasurementApp.Inches(68.0);
+		
+		assertFalse(f1.equals(null));
+	}
+	
+	@Test
+	void testInchEquality_NonNumericInput() {
+		QuantityMeasurementApp.Inches f1=new QuantityMeasurementApp.Inches(68.0);
+		
+		assertFalse(f1.equals("68"));
+	}
+	
+	@Test
+	void testInchEquality_SameReference() {
+		QuantityMeasurementApp.Inches f1=new QuantityMeasurementApp.Inches(68.0);
+		
+		assertTrue(f1.equals(f1));
+	}
+	
+	@Test
+    void testInchEquality_Consistent() {
+		QuantityMeasurementApp.Inches f1 = new QuantityMeasurementApp.Inches(1.0);
+		QuantityMeasurementApp.Inches f2 = new QuantityMeasurementApp.Inches(1.0);
 
         assertTrue(f1.equals(f2));
         assertTrue(f1.equals(f2));
